c9ac9335e00bbbb1abe2019b072689db7e1a06b4
phjppo0918 <phjppo0918@gmail.com>
2023-10-29 20:35:42 +0900

    fix : 코드 수정

--- src/main/java/com/example/dbschool/domain/AnswerComment.java
+++ src/main/java/com/example/dbschool/domain/AnswerComment.java
@@ -1,8 +1,12 @@
 package com.example.dbschool.domain;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.Data;
 
+@Entity
+@Data
 public class AnswerComment {
     @Id
     Long id;
